15b9d74299fcec2b3042ab557cd3263d976d665f
bendou <sami.dahoux@etu.emse.fr>
2020-01-14 13:50:43 +0100

    Commenting keymap

--- src/keys.rs
+++ src/keys.rs
@@ -2,21 +2,25 @@ use piston::input::{Key, MouseButton};
 
 pub static KEY_RESET: Key = Key::Backspace;
 
+// Config toggles
 pub static KEY_TOGGLE_TRANSLATE: Key = Key::J;
 pub static KEY_TOGGLE_TRAJECTORY: Key = Key::R;
 pub static KEY_TOGGLE_PAUSE: Key = Key::Space;
 pub static KEY_TOGGLE_ORBITS: Key = Key::Y;
 
+// Translation directions
 pub static KEY_DIRECTION_UP: Key = Key::W;
 pub static KEY_DIRECTION_DOWN: Key = Key::S;
 pub static KEY_DIRECTION_LEFT: Key = Key::A;
 pub static KEY_DIRECTION_RIGHT: Key = Key::D;
 
+// Rotation orientations
 pub static KEY_ROTATION_UP: Key = Key::Up;
 pub static KEY_ROTATION_DOWN: Key = Key::Down;
 pub static KEY_ROTATION_LEFT: Key = Key::Left;
 pub static KEY_ROTATION_RIGHT: Key = Key::Right;
 
+// Scaling
 pub static KEY_INCREASE_OVERSAMPLING: Key = Key::P;
 pub static KEY_DECREASE_OVERSAMPLING: Key = Key::O;
 
@@ -29,6 +33,7 @@ pub static KEY_DECREASE_TIME: Key = Key::M;
 pub static KEY_INCREASE_CURRENT_INDEX: Key = Key::V;
 pub static KEY_DECREASE_CURRENT_INDEX: Key = Key::C;
 
+// State transitions
 pub static KEY_NEXT_LOGGER_STATE: Key = Key::L;
 pub static KEY_NEXT_FRAME_STATE: Key = Key::K;
 pub static KEY_NEXT_METHOD_STATE: Key = Key::Semicolon;
